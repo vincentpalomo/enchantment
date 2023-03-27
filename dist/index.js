77673ca61ce6bfb109e6c06d157701a3d81bf47d
Vincent Palomo <vincentpalomo@yahoo.com>
2023-03-26 23:12:49 -0500

    ran tsc

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -7,7 +16,6 @@ const dotenv_1 = require("dotenv");
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const http_errors_1 = __importDefault(require("http-errors"));
-// const router = require('./api')
 // dotenv
 (0, dotenv_1.config)();
 // express server
@@ -32,5 +40,19 @@ const errorHandler = (err, req, res, next) => {
 };
 app.use(errorHandler);
 // api router
-// app.use('/api', router)
-module.exports = app;
+app.use('/api', require('./api'));
+// DB connection
+const { client } = require('./db');
+// connect to server
+const PORT = process.env.PORT || 4000;
+const handle = app.listen(PORT, () => __awaiter(void 0, void 0, void 0, function* () {
+    console.log(`⚡ Server running on ${PORT}`);
+    try {
+        yield client.connect();
+        console.log(`Database is online! 🔥`);
+    }
+    catch (error) {
+        console.error('Database is not online! 😵');
+    }
+}));
+module.exports = { app, handle };
