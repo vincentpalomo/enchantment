f8a42825d0c462f00ae98f3dc1125ae03dce9d13
vincent <vincentpalomo@yahoo.com>
2023-03-30 19:18:36 -0500

    added card boilerplate

---
+++ src/components/Cards.tsx
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+
+type Props = {};
+
+const Cards: React.FC = (props: Props) => {
+  const [cards, setCards] = useState("");
+  // fetch all cards
+  // store in state
+  // map state
+
+  return (
+    <>
+      <div>Cards</div>
+    </>
+  );
+};
+
+export default Cards;
