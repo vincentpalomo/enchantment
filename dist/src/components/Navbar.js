17b41d28794336e7ff3b6a86c7924835efd844cb
vincent <vincentpalomo@yahoo.com>
2023-04-07 00:10:44 -0500

    created mobile dropdown menu currently not in the correct side

--- dist/src/components/Navbar.js
+++ dist/src/components/Navbar.js
@@ -1,24 +1,69 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (k !== "default" && Object.prototype.hasOwnProperty.call(mod, k)) __createBinding(result, mod, k);
+    __setModuleDefault(result, mod);
+    return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const react_1 = __importDefault(require("react"));
+const react_1 = __importStar(require("react"));
 const react_router_dom_1 = require("react-router-dom");
 const Navbar = (props) => {
+    const [isMenuOpen, setIsMenuOpen] = (0, react_1.useState)(false);
     const navigate = (0, react_router_dom_1.useNavigate)();
     const logout = () => {
         props.setUser('');
         props.setOnline(false);
         localStorage.removeItem('user');
         navigate('/');
+        setIsMenuOpen(false);
     };
     console.log(props.online);
-    return (react_1.default.createElement("div", { className: 'flex items-center justify-between h-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-8' },
+    const handleToggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+    const handleCloseMenu = () => {
+        setIsMenuOpen(false);
+    };
+    return (react_1.default.createElement("div", { className: 'relative z-50 flex items-center justify-between h-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-8' },
         react_1.default.createElement("div", { className: 'flex items-center' },
             react_1.default.createElement(react_router_dom_1.Link, { to: '/cards' },
-                react_1.default.createElement("span", { className: 'font-serif text-4xl text-black' }, "enchantment \uD83E\uDDD9\u200D\u2642\uFE0F"))),
-        react_1.default.createElement("div", { className: 'flex justify-end flex-1 mr-auto' },
+                react_1.default.createElement("span", { className: 'font-serif text-4xl text-black' }, "enchantment \uD83E\uDDD9\u200D\u2642\uFE0F")),
+            react_1.default.createElement("div", { className: `${isMenuOpen ? 'block' : 'hidden'} lg:hidden absolute top-16 bg-white py-2 px-4` },
+                react_1.default.createElement(react_router_dom_1.Link, { to: '/', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "home"),
+                react_1.default.createElement(react_router_dom_1.Link, { to: '/cards', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "cards"),
+                react_1.default.createElement(react_router_dom_1.Link, { to: '/decks', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "decks"),
+                react_1.default.createElement(react_router_dom_1.Link, { to: '/about', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "about"),
+                !props.online ? (react_1.default.createElement(react_1.default.Fragment, null,
+                    react_1.default.createElement(react_router_dom_1.Link, { to: '/login', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "login"),
+                    react_1.default.createElement(react_router_dom_1.Link, { to: '/register', className: 'block font-serif text-xl text-black hover:text-pink-500', onClick: handleCloseMenu }, "register"))) : (react_1.default.createElement(react_1.default.Fragment, null,
+                    react_1.default.createElement("div", { className: 'flex' },
+                        react_1.default.createElement("img", { alt: 'avatar', src: 'https://i.ibb.co/F0nkS0z/Avatar.png', className: 'object-cover w-10 h-10 rounded-full' }),
+                        react_1.default.createElement("span", { className: 'block m-1 font-serif text-xl text-pink-500' }, props.user)),
+                    react_1.default.createElement(react_router_dom_1.Link, { to: '/', className: 'active:text-pink-500' },
+                        react_1.default.createElement("button", { onClick: logout, className: 'block font-serif text-xl text-black hover:text-pink-500' }, "logout"))))),
+            react_1.default.createElement("button", { className: 'block mr-auto text-gray-800 lg:hidden hover:text-gray-600 focus:text-gray-600', onClick: handleToggleMenu }, "\uD83E\uDDD9\u200D\u2642\uFE0F")),
+        react_1.default.createElement("div", { className: 'flex items-center' },
+            react_1.default.createElement(react_router_dom_1.Link, { to: '/cards' },
+                react_1.default.createElement("span", { className: 'hidden font-serif text-4xl text-black' }, "enchantment \uD83E\uDDD9\u200D\u2642\uFE0F"))),
+        react_1.default.createElement("div", { className: 'justify-end flex-1 hidden mr-auto lg:flex' },
             react_1.default.createElement(react_router_dom_1.Link, { to: '/', className: 'active:text-pink-500' },
                 react_1.default.createElement("button", { className: 'm-2 font-serif text-xl text-black hover:text-pink-500 focus:text-pink-500' }, "home")),
             react_1.default.createElement(react_router_dom_1.Link, { to: '/cards', className: 'active:text-pink-500' },
@@ -27,7 +72,7 @@ const Navbar = (props) => {
                 react_1.default.createElement("button", { className: 'm-2 font-serif text-xl text-black hover:text-pink-500 focus:text-pink-500' }, "decks")),
             react_1.default.createElement(react_router_dom_1.Link, { to: '/about', className: ' active:text-pink-500' },
                 react_1.default.createElement("button", { className: 'm-2 font-serif text-xl text-black hover:text-pink-500 focus:text-pink-500' }, "about"))),
-        react_1.default.createElement("div", { className: 'flex items-center justify-end flex-1' }, !props.online ? (react_1.default.createElement(react_1.default.Fragment, null,
+        react_1.default.createElement("div", { className: 'items-center justify-end flex-1 hidden lg:flex' }, !props.online ? (react_1.default.createElement(react_1.default.Fragment, null,
             react_1.default.createElement(react_router_dom_1.Link, { to: '/login', className: ' active:text-pink-500' },
                 react_1.default.createElement("button", { className: 'm-2 font-serif text-xl text-black hover:text-pink-500 focus:text-pink-500' }, "login")),
             react_1.default.createElement(react_router_dom_1.Link, { to: '/register', className: ' active:text-pink-500' },
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
 type Props = {
@@ -9,6 +9,8 @@ type Props = {
 };
 
 const Navbar = (props: Props) => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   const navigate = useNavigate();
 
   const logout = () => {
@@ -16,16 +18,104 @@ const Navbar = (props: Props) => {
     props.setOnline(false);
     localStorage.removeItem('user');
     navigate('/');
+    setIsMenuOpen(false);
   };
+
   console.log(props.online);
+
+  const handleToggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
+  const handleCloseMenu = () => {
+    setIsMenuOpen(false);
+  };
+
   return (
-    <div className='flex items-center justify-between h-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-8'>
+    <div className='relative z-50 flex items-center justify-between h-16 mx-auto max-w-screen-2xl sm:px-6 lg:px-8'>
       <div className='flex items-center'>
         <Link to='/cards'>
           <span className='font-serif text-4xl text-black'>enchantment 🧙‍♂️</span>
         </Link>
+
+        <div
+          className={`${
+            isMenuOpen ? 'block' : 'hidden'
+          } lg:hidden absolute top-16 bg-white py-2 px-4`}>
+          <Link
+            to='/'
+            className='block font-serif text-xl text-black hover:text-pink-500'
+            onClick={handleCloseMenu}>
+            home
+          </Link>
+          <Link
+            to='/cards'
+            className='block font-serif text-xl text-black hover:text-pink-500'
+            onClick={handleCloseMenu}>
+            cards
+          </Link>
+          <Link
+            to='/decks'
+            className='block font-serif text-xl text-black hover:text-pink-500'
+            onClick={handleCloseMenu}>
+            decks
+          </Link>
+          <Link
+            to='/about'
+            className='block font-serif text-xl text-black hover:text-pink-500'
+            onClick={handleCloseMenu}>
+            about
+          </Link>
+          {!props.online ? (
+            <>
+              <Link
+                to='/login'
+                className='block font-serif text-xl text-black hover:text-pink-500'
+                onClick={handleCloseMenu}>
+                login
+              </Link>
+              <Link
+                to='/register'
+                className='block font-serif text-xl text-black hover:text-pink-500'
+                onClick={handleCloseMenu}>
+                register
+              </Link>
+            </>
+          ) : (
+            <>
+              <div className='flex'>
+                <img
+                  alt='avatar'
+                  src='https://i.ibb.co/F0nkS0z/Avatar.png'
+                  className='object-cover w-10 h-10 rounded-full'
+                />
+                <span className='block m-1 font-serif text-xl text-pink-500'>{props.user}</span>
+              </div>
+              <Link to='/' className='active:text-pink-500'>
+                <button
+                  onClick={logout}
+                  className='block font-serif text-xl text-black hover:text-pink-500'>
+                  logout
+                </button>
+              </Link>
+            </>
+          )}
+        </div>
+
+        <button
+          className='block mr-auto text-gray-800 lg:hidden hover:text-gray-600 focus:text-gray-600'
+          onClick={handleToggleMenu} // Add your function to handle toggling the menu
+        >
+          🧙‍♂️
+        </button>
       </div>
-      <div className='flex justify-end flex-1 mr-auto'>
+
+      <div className='flex items-center'>
+        <Link to='/cards'>
+          <span className='hidden font-serif text-4xl text-black'>enchantment 🧙‍♂️</span>
+        </Link>
+      </div>
+      <div className='justify-end flex-1 hidden mr-auto lg:flex'>
         <Link to='/' className='active:text-pink-500'>
           <button className='m-2 font-serif text-xl text-black hover:text-pink-500 focus:text-pink-500'>
             home
@@ -48,7 +138,7 @@ const Navbar = (props: Props) => {
           </button>
         </Link>
       </div>
-      <div className='flex items-center justify-end flex-1'>
+      <div className='items-center justify-end flex-1 hidden lg:flex'>
         {!props.online ? (
           <>
             <Link to='/login' className=' active:text-pink-500'>
