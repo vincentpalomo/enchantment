10aee064d75293af5cf347c4c7d8dd652a5eb361
vincent <vincentpalomo@yahoo.com>
2023-04-07 23:15:38 -0500

    updated navbar with correct user avatar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -169,11 +169,17 @@ const Navbar = (props: Props) => {
           </>
         ) : (
           <>
-            <img
+            {/* <img
               alt='avatar'
               src='https://i.ibb.co/F0nkS0z/Avatar.png'
               className='object-cover w-10 h-10 rounded-full'
+            /> */}
+            <img
+              src={props.loggedUser?.avatar}
+              alt=''
+              className='object-cover w-10 h-10 rounded-full'
             />
+
             <span className='m-2 font-serif text-xl text-pink-500'>{props.user}</span>
             <Link to='/' className='active:text-pink-500'>
               <button
